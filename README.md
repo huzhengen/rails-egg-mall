b5f5d2fcd13c3c92dec72ce7832a09bf07af4601
huzhengen <huzhengen@163.com>
2021-12-27 18:22:43 +0800

    Register and login

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ bin/rails g sorcery:install
 bin/rails generate sorcery:install user_activation remember_me reset_password --only-submodules
 ```
 
+Mac 上3000 端口被占用
+
+```
+lsof -i :3000
+kill -9 <PID>
+```
+
+```
+bin/rails g mailer user
+```
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  attr_accessor :password, :password_confirmation
+
+  validates_presence_of :email, message: "邮箱不能为空"
+  validates_format_of :email, message: "邮箱格式不正确", with: /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/, if: proc { |user| !user.email.blank? }
+  validates :email, uniqueness: true
+
+  validates_presence_of :password, message: "密码不能为空", if: :need_password_validate
+  validates_presence_of :password_confirmation, message: "密码确认不能为空", if: :need_password_validate
+  validates_confirmation_of :password, message: "两次密码输入不一致", if: :need_password_validate
+  validates_length_of :password, message: "密码长度不能少于6位", minimum: 6, if: :need_password_validate
+
+  def username
+    self.email.split('@').first
+  end
+
+  private
+    def need_password_validate
+      self.new_record? || (!self.password.nil? || !self.password_confirmation.nil?)
+    end
 end
