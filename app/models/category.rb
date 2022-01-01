d6821c517f97ed35a733960a99557b5a78507995
huzhengen <huzhengen@163.com>
2022-01-01 18:49:42 +0800

    Finish admin category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,11 +1,13 @@
 class Category < ApplicationRecord
-  validates :title, presence: {message: "名称不能为空Ï"}
-  has_ancestry
+  validates :title, presence: { message: "名称不能为空Ï" }
+  validates :title, uniqueness: { message: "名称不能重复Ï" }
+  has_ancestry orphan_strategy: :destroy
   has_many :products, dependent: :destroy
 
   before_validation :correct_ancestry
 
   private
+
   def correct_ancestry
     self.ancestry = nil if self.ancestry.blank?
   end
