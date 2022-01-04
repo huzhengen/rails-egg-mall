a51482834b77966cb69bdd9ab8f2239a1d52be3d
huzhengen <huzhengen@163.com>
2022-01-04 15:53:44 +0800

    Product validates

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     root 'sessions#new'
     resources :sessions
     resources :categories
+    resources :products
   end
   root 'welcome#index'
   resources :users
