2c372d14fc540f7084510f1715bf727484eda3e3
huzhengen <huzhengen@163.com>
2022-01-04 19:41:41 +0800

    Update weight of product image

--- app/controllers/admin/product_images_controller.rb
+++ app/controllers/admin/product_images_controller.rb
@@ -13,6 +13,17 @@ class Admin::ProductImagesController < Admin::BaseController
     redirect_back fallback_location: root_path
   end
 
+  def update
+    @product_image = @product.product_images.find(params[:id])
+    @product_image.weight = params[:weight]
+    if @product_image.save
+      flash[:notice] = "修改权重成功"
+    else
+      flash[:notice] = "修改权重失败"
+    end
+    redirect_back fallback_location: root_path
+  end
+
   def destroy
     @product_image = @product.product_images.find(params[:id])
     if @product_image.destroy
