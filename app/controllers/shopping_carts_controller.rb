ee332cf7f533604cf0697e49989e3f9eee104987
huzhengen <huzhengen@163.com>
2022-01-13 15:19:44 +0800

    Shopping cart

---
+++ app/controllers/shopping_carts_controller.rb
@@ -0,0 +1,22 @@
+class ShoppingCartsController < ApplicationController
+
+  def index
+    fetch_home_data
+    @shopping_carts = ShoppingCart.by_user_uuid(session[:uuid]).order('id desc')
+                                  .includes([:product => [:main_product_image]])
+  end
+
+  def create
+    amount = params[:amount].to_i
+    amount = amount <= 0 ? 1 : amount
+
+    @product = Product.find(params[:product_id])
+    @shopping_cart = ShoppingCart.create_or_update!({
+                                                      user_uuid: session[:user_uuid],
+                                                      product_id: params[:product_id],
+                                                      amount: amount
+                                                    })
+    render layout: false
+  end
+
+end
